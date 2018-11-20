00458a617e74d5a84b2f1fc932c70140ec098c88
mkduchak <michaelkduchak@gmail.com>
2018-11-19 22:15:49 -0600

    added logging to the aws script

--- scraper/aws_script.sh
+++ scraper/aws_script.sh
@@ -2,12 +2,16 @@
 
 # convert ipython notebook into python script
 jupyter nbconvert --to python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.ipynb
+echo "Converted Python"
 
 # runs newly created python script
 python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.py
+echo "Executed Python"
 
 # Moves all standings to dropbox uploader
 cp -r /home/ubuntu/fantasy_basketball_project/scraper/data/standings/ /home/ubuntu/Dropbox-Uploader/
+echo "Moved Files to Dropbox"
 
 # Uploads files in dropbox uploader
 /home/ubuntu/Dropbox-Uploader/dropbox_uploader.sh -s upload standings/ basketball_standings/
+echo "Uploaded new files to dropbox"

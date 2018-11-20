875eb004e3c0a8716ecabf69db3e8f765d8b8941
mkduchak <michaelkduchak@gmail.com>
2018-11-19 22:09:12 -0600

    again

--- scraper/aws_script.sh
+++ scraper/aws_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # convert ipython notebook into python script
-jupyter nbconvert --to python team_points_summary.ipynb
+jupyter nbconvert --to python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.ipynb
 
 # runs newly created python script
-python team_points_summary.py
+python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.py
 
 # Moves all standings to dropbox uploader
 cp -r /home/ubuntu/fantasy_basketball_project/scraper/data/standings/ /home/ubuntu/Dropbox-Uploader/

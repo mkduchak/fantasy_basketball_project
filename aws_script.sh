eb4c8cd315801b0e9b44242a371df7b703ec935c
mkduchak <michaelkduchak@gmail.com>
2018-11-19 21:56:43 -0600

    updated pathing to current directory to the right location

---
+++ aws_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd fantasy_basketball_project/scraper/
+# convert ipython notebook into python script
+jupyter nbconvert --to python team_points_summary.ipynb
+
+# runs newly created python script
+python team_points_summary.py
+
+# Moves all standings to dropbox uploader
+cp -r /home/ubuntu/fantasy_basketball_project/scraper/data/standings/ /home/ubuntu/Dropbox-Uploader/
+
+# Uploads files in dropbox uploader
+/home/ubuntu/Dropbox-Uploader/dropbox_uploader.sh -s upload standings/ basketball_standings/

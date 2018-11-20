3d3ea9c6da19159e469dfe3846ff5e8de8efcc9b
mkduchak <michaelkduchak@gmail.com>
2018-11-19 22:01:45 -0600

    updated code to fix pathing of chromedriver

--- aws_script.sh
+++ aws_script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-cd fantasy_basketball_project/scraper/
 # convert ipython notebook into python script
-jupyter nbconvert --to python team_points_summary.ipynb
+jupyter nbconvert --to python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.ipynb
 
 # runs newly created python script
-python team_points_summary.py
-
-cd ~
+python /home/ubuntu/fantasy_basketball_project/scraper/team_points_summary.py
 
 # Moves all standings to dropbox uploader
 cp -r /home/ubuntu/fantasy_basketball_project/scraper/data/standings/ /home/ubuntu/Dropbox-Uploader/

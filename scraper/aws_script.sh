3fc7b41784a07117e90f502ada76614874b3581c
mkduchak <michaelkduchak@gmail.com>
2018-11-27 15:48:42 -0600

    added lines to kill all chrome and chromedriver applications to free up memory

--- scraper/aws_script.sh
+++ scraper/aws_script.sh
@@ -15,3 +15,7 @@ echo "Moved Files to Dropbox"
 # Uploads files in dropbox uploader
 /home/ubuntu/Dropbox-Uploader/dropbox_uploader.sh -s upload Dropbox-Uploader/standings/ basketball_standings/
 echo "Uploaded new files to dropbox"
+
+# Killall chrome and chromedrivers
+killall /home/ubuntu/fantasy_basketball_project/chromedriver
+killall /opt/google/chrome/chrome
